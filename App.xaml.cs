9cce2516da9f4986d0422ad2f3988e52a98ccdd1
xiejiang2014 <44390984+xiejiang2014@users.noreply.github.com>
2022-04-30 21:22:05 +0800

    .

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Xiejiang.SKLottie.Samples.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace Xiejiang.SKLottie.Samples.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnDragEnter
+        (
+            object        sender,
+            DragEventArgs e
+        )
+        {
+            e.Effects = e.Data.GetDataPresent
+                            (
+                             DataFormats.FileDrop
+                            ) ?
+                            DragDropEffects.Link :
+                            DragDropEffects.None;
+        }
+
+
+        private async void MainWindow_OnDrop
+        (
+            object        sender,
+            DragEventArgs e
+        )
+        {
+            if (e.Data.GetData
+                    (
+                     DataFormats.FileDrop
+                    ) is string[] fileList && fileList.Any())
+            {
+                LottieFile = fileList.First();
+            }
+        }
+
+
+        private string _lottieFile;
+
+        public string LottieFile
+        {
+            get => _lottieFile;
+            set
+            {
+                _lottieFile = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Play_OnClick
+        (
+            object          sender,
+            RoutedEventArgs e
+        )
+        {
+            LottieViewer.IsPlaying = true;
+        }
+
+        private void Stop_OnClick
+        (
+            object          sender,
+            RoutedEventArgs e
+        )
+        {
+            LottieViewer.IsPlaying = false;
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged
+        (
+            [CallerMemberName] string? propertyName = null
+        )
+        {
+            PropertyChanged?.Invoke
+                (
+                 this,
+                 new PropertyChangedEventArgs
+                     (
+                      propertyName
+                     )
+                );
+        }
+    }
+}
